f9921fcb3ec3b73d0e78154ea092f67e6ab64b6d
developer_seremet <seremet@luum.ms>
2015-05-04 16:23:19 +0200

    Fogio now capable of multiple credentials

--- fogio-cmd.php
+++ fogio-cmd.php
@@ -1,9 +1,23 @@
 <?php
+define("PATH_TO_CREDENTIALS", "./app/Fogio/config/");
+
+if ($argc < 1) {
+    print "\nCredentials needed. Stopping!\n";
+    exit();
+}
+
+$credentials_called = $argv[1];
+$credentials_config_file = $argv[1] . 'php';
+
+if (!is_file(PATH_TO_CREDENTIALS . $credentials_config_file)) {
+    print "\nNo such credentials found. Stopping!\n";
+    exit;
+}
 
 use Fogio\Fogio;
 
 require_once './vendor/autoload.php';
-require_once './app/Fogio/config/credentials.php';
+require_once PATH_TO_CREDENTIALS . $credentials_config_file;
 
 $fogio = new Fogio($credentials);
 
